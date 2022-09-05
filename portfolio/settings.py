ad639f1cabd3387c30e1f460b456a4efa3a9840d
Wallace-p-p <wallacepncp@gmail.com>
2022-09-04 23:34:19 -0300

    final v

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('secret3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    '*',
     'portfolio-wallacepp.herokuapp.com',
     '127.0.0.1',
 ]
